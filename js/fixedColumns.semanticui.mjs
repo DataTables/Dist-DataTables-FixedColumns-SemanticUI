af0b3d9ab2ff7bf2b24101e8b1f7a574946c1b2e
Allan Jardine <allan@datatables.net>
2022-10-10 19:49:16 +0000

    Add support for mjs files (ES Modules)

---
+++ js/fixedColumns.semanticui.mjs
@@ -0,0 +1,13 @@
+
+/*! Semantic ui integration for DataTables' FixedColumns
+ * © SpryMedia Ltd - datatables.net/license
+ */
+
+import $ from 'jquery';
+import DataTable from 'datatables.net-se';
+import DataTable from 'datatables.net-fixedcolumns';
+
+
+
+
+export default DataTable;
