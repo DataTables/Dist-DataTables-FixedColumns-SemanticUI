c6c9698ff9ffbd178380f6392912dc0b19f48c0a
Allan Jardine <allan.jardine@sprymedia.co.uk>
2022-11-04 17:09:08 +0000

    865d9d775c4c8dc61e9159e44084f30a2b664752
    Sync to source repo @865d9d775c4c8dc61e9159e44084f30a2b664752

--- js/fixedColumns.semanticui.min.mjs
+++ js/fixedColumns.semanticui.min.mjs
@@ -1,1 +1,4 @@
+/*! Semantic ui integration for DataTables' FixedColumns
+ * © SpryMedia Ltd - datatables.net/license
+ */
 import $ from"jquery";import DataTable from"datatables.net-se";import DataTable from"datatables.net-fixedcolumns";export default DataTable;
