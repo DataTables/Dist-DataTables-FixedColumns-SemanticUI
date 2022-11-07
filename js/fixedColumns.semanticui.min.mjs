72bad208c0859d5e36bc180e18bfe1f57e3cd395
Allan Jardine <allan.jardine@sprymedia.co.uk>
2022-11-07 16:13:06 +0000

    7bc6dcd5053b2d6128798325bbdf202dc14ef38c Release 4.2.1 (packaging error with 4.2.0)
    Sync to source repo @7bc6dcd5053b2d6128798325bbdf202dc14ef38c

--- js/fixedColumns.semanticui.min.mjs
+++ js/fixedColumns.semanticui.min.mjs
@@ -1,4 +1,4 @@
 /*! Semantic ui integration for DataTables' FixedColumns
  * © SpryMedia Ltd - datatables.net/license
  */
-import $ from"jquery";import DataTable from"datatables.net-se";import DataTable from"datatables.net-fixedcolumns";export default DataTable;
+import $ from"jquery";import DataTable from"datatables.net-se";import"datatables.net-fixedcolumns";export default DataTable;
--- js/fixedColumns.semanticui.mjs
+++ js/fixedColumns.semanticui.mjs
@@ -1,11 +1,10 @@
-
 /*! Semantic ui integration for DataTables' FixedColumns
  * © SpryMedia Ltd - datatables.net/license
  */
 
 import $ from 'jquery';
 import DataTable from 'datatables.net-se';
-import DataTable from 'datatables.net-fixedcolumns';
+import 'datatables.net-fixedcolumns';
 
 
 
